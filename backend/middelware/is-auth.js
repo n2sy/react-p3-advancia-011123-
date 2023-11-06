bcecb96aed103965668adcf0c2c5257fc6be3e97
Nidhal <nidhal@N2SY.local>
2023-11-06 10:41:55 +0100

    add getcandidatbyid

--- backend/middelware/is-auth.js
+++ backend/middelware/is-auth.js
@@ -1,29 +1,25 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = async (req, res, next) => {
-    const authHeader = req.get('Authorization');
-    if (!authHeader) {
-        const error = new Error("Not authenticated");
-        error.statusCode = 401;
-        throw error;
-    }
-    const token = authHeader.split(' ')[1];
+  const authHeader = req.get("Authorization");
+  if (!authHeader) {
+    const error = new Error("Not authenticated");
+    error.statusCode = 401;
+    res.status(401).json({ message: "Not Authenticated" });
+  } else {
+    const token = authHeader.split(" ")[1];
 
     let decodedToken;
     try {
-        decodedToken = jwt.verify(token, 'supersecretcode') // decode and verify
-    }
-    catch (err) {
-        throw err;
+      decodedToken = jwt.verify(token, "supersecretcode"); // decode and verify
+    } catch (err) {
+      res.status(401).json({ err });
     }
 
     if (!decodedToken) {
-        const error = new Error("Not authenticated");
-        error.statusCode = 401;
-        throw error;
-
-    }
-
-    next();
-
-}
+      const error = new Error("Not authenticated");
+      error.statusCode = 401;
+      next(error);
+    } else next();
+  }
+};
